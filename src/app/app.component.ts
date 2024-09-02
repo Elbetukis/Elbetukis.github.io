fb986aaae3de9d1ad0723589cb5acd5acfb49706
elbetukis <albert10xd@gmail.com>
2024-09-02 12:22:10 -0600

    work experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
